8bb0ea7d99d50eddb2be4331563bc52ba1a7a12f
Blue Teeth <daominhtri1000@gmail.com>
2024-03-03 16:10:55 +0700

    Add row version database

--- KidsPro/Domain/Entities/Generic/BaseEntity.cs
+++ KidsPro/Domain/Entities/Generic/BaseEntity.cs
@@ -8,5 +8,6 @@ public class BaseEntity
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
+    [Timestamp] public byte[] Version { get; set; } = null!;
 }
