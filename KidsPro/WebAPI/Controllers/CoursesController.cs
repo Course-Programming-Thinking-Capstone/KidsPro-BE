8543fb0449a8b6e986577060f84c333220777dd3
Blue Teeth <daominhtri1000@gmail.com>
2024-03-17 00:12:07 +0700

    update course authorize

--- KidsPro/WebAPI/Controllers/CoursesController.cs
+++ KidsPro/WebAPI/Controllers/CoursesController.cs
@@ -30,6 +30,7 @@ public class CoursesController : ControllerBase
     /// <param name="id"></param>
     /// <returns></returns>
     [HttpGet("{id:int}")]
+    [Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole},{Constant.StaffRole}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CourseDto))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDetail))]
     public async Task<ActionResult<CourseDto>> GetByIdAsync([FromRoute] int id)
@@ -68,7 +69,7 @@ public class CoursesController : ControllerBase
     /// <param name="dto"></param>
     /// <returns></returns>
     [HttpPost]
-    //[Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole},{Constant.StaffRole}")]
+    [Authorize(Roles = $"{Constant.AdminRole}")]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CourseDto))]
     [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ErrorDetail))]
     [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorDetail))]
@@ -86,7 +87,7 @@ public class CoursesController : ControllerBase
     /// <param name="dto"></param>
     /// <param name="action"></param>
     /// <returns></returns>
-    [Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole},{Constant.StaffRole}")]
+    [Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole}")]
     [HttpPut("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CourseDto))]
     [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ErrorDetail))]
@@ -105,7 +106,7 @@ public class CoursesController : ControllerBase
     /// <param name="id"></param>
     /// <param name="file"></param>
     /// <returns></returns>
-    [Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole},{Constant.StaffRole}")]
+    [Authorize(Roles = $"{Constant.AdminRole},{Constant.TeacherRole}")]
     [HttpPatch("{id:int}/picture")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CourseDto))]
     public async Task<ActionResult<CourseDto>> UpdateCoursePictureAsync([FromRoute] int id, [FromForm] IFormFile file)
