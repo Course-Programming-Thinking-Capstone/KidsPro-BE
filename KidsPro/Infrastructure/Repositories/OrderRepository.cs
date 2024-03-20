3c1733eb9948ed3c27f70e09bc894fe038ad8c35
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2024-03-20 15:51:51 +0700

    update

--- KidsPro/Infrastructure/Repositories/OrderRepository.cs
+++ KidsPro/Infrastructure/Repositories/OrderRepository.cs
@@ -16,71 +16,50 @@ namespace Infrastructure.Repositories
         {
         }
 
-        public async Task<(Order?,string?)> GetByOrderCode(Func<int,string> generateOrderCode, bool decision)
+        public async Task<(Order?, string?)> GetByOrderCode(Func<int, string> generateOrderCode, bool decision)
         {
             string orderCode = generateOrderCode(13);
-           
+
             switch (decision)
             {
                 // Search By OrderCode
                 case true:
-                    return (await _dbSet.Include(x=> x.OrderDetails)!.ThenInclude(x=> x.Course)
-                            .FirstOrDefaultAsync(x=> x.OrderCode == orderCode),null);
+                    return (await _dbSet.Include(x => x.OrderDetails)!.ThenInclude(x => x.Course)
+                        .FirstOrDefaultAsync(x => x.OrderCode == orderCode), null);
                 // tìm kiếm xem order code có tồn tại trong system chưa, truyền vào false, nếu mà tìm không có bằng null thì trả về 
                 // theo order code để tạo mới order, còn nếu trùng thì trả về 1 order để check rồi tạo orderCode khác
                 case false:
-                    return (await _dbSet.FirstOrDefaultAsync(x => x.OrderCode == orderCode),orderCode);
+                    return (await _dbSet.FirstOrDefaultAsync(x => x.OrderCode == orderCode), orderCode);
             }
         }
 
-        public async Task<Order?> GetOrderByStatusAsync(int parentId, int orderId,OrderStatus status)
+        public async Task<Order?> GetOrderByStatusAsync(int parentId, int orderId, OrderStatus status)
         {
             return await _dbSet.AsNoTracking().FirstOrDefaultAsync
-                (x=> x.Id == orderId && x.ParentId==parentId && x.Status == status);
+                (x => x.Id == orderId && x.ParentId == parentId && x.Status == status);
         }
 
-        public async Task<List<Order>?> GetListOrderAsync(OrderStatus status,int parentId,string role)
+        public async Task<List<Order>?> GetListOrderAsync(OrderStatus status, int parentId, string role)
         {
             var query = _dbSet.AsNoTracking();
             if (role == Constant.ParentRole)
             {
                 query = query.Where(x => x.ParentId == parentId);
             }
-            switch (status)
-            {
-                case OrderStatus.Payment:
-                    return await query.Where(x => x.Status == OrderStatus.Payment)
-                        .Include(x=> x.Parent).ThenInclude(x=>x.Account)
-                        .Include(x=>x.OrderDetails)!.ThenInclude(x=>x.Course).ToListAsync();
-                case OrderStatus.Pending:
-                    return await query.Where(x => x.Status == OrderStatus.Pending)
-                        .Include(x=> x.Parent).ThenInclude(x=>x.Account)
-                        .Include(x=>x.OrderDetails)!.ThenInclude(x=>x.Course).ToListAsync();
-                case OrderStatus.Success:
-                    return await query.Where(x => x.Status == OrderStatus.Success)
-                        .Include(x=> x.Parent).ThenInclude(x=>x.Account)
-                        .Include(x=>x.OrderDetails)!.ThenInclude(x=>x.Course).ToListAsync();
-                case OrderStatus.RequestRefund:
-                    return await query.Where(x => x.Status == OrderStatus.RequestRefund)
-                        .Include(x=> x.Parent).ThenInclude(x=>x.Account)
-                        .Include(x=>x.OrderDetails)!.ThenInclude(x=>x.Course).ToListAsync();
-                case OrderStatus.Refunded:
-                    return await query.Where(x => x.Status == OrderStatus.Refunded)
-                        .Include(x=> x.Parent).ThenInclude(x=>x.Account)
-                        .Include(x=>x.OrderDetails)!.ThenInclude(x=>x.Course).ToListAsync();
-            }
-            throw new UnauthorizedException("Parent Id doesn't exist");
+
+            return await query.Where(x => x.Status == status)
+                .Include(x => x.Parent).ThenInclude(x => x!.Account)
+                .Include(x => x.OrderDetails)!.ThenInclude(x => x.Course).ToListAsync();
         }
 
         public async Task<Order?> GetOrderDetail(int parentId, int orderId)
         {
             return (await _dbSet.AsNoTracking().Include(x => x.OrderDetails)!
-                .ThenInclude(x=> x.Students)!.ThenInclude(x=> x.Account)
+                .ThenInclude(x => x.Students)!.ThenInclude(x => x.Account)
                 .Include(x => x.OrderDetails)!.ThenInclude(x => x.Course)
                 .Include(x => x.Transaction).Include(x => x.Voucher)
-                .Include(x=>x.Parent).ThenInclude(x=>x!.Account)
+                .Include(x => x.Parent).ThenInclude(x => x!.Account)
                 .FirstOrDefaultAsync(x => x.Id == orderId && x.ParentId == parentId));
         }
-        
     }
 }
