d31610fd69c601df0cc1bb6faf10e3ce5c64d0b6
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2024-03-24 11:59:52 +0700

    fix create student account

--- KidsPro/Application/Services/StaffService.cs
+++ KidsPro/Application/Services/StaffService.cs
@@ -7,7 +7,7 @@ using Domain.Enums;
 
 namespace Application.Services;
 
-public class StaffService:IStaffService
+public class StaffService : IStaffService
 {
     private IUnitOfWork _unitOfWork;
     private IAccountService _accountService;
@@ -24,40 +24,47 @@ public class StaffService:IStaffService
     {
         var account = await _accountService.GetCurrentAccountInformationAsync();
 
-        if (account.Role == Constant.StaffRole)
-        {
-            var student = await _unitOfWork.StudentRepository.GetByIdAsync(dto.StudendId);
-            if (student != null)
-            {
-                student.UserName = dto.UserName;
-                student.Account.PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(dto.Password);
-                _unitOfWork.StudentRepository.Update(student);
-                var result= await _unitOfWork.SaveChangeAsync();
-                if (result > 0 &&
-                    await _unitOfWork.AccountRepository.ExistByEmailAsync(dto.ParentEmail!)!=null)
-                {
-                    //Send Email
-                    var toSubject = "KidsPro Send Student Account";
-                    var toContent = "Student Name: " + student.Account.FullName + "<br>" + 
-                                    "Birthday: " + student.Account.DateOfBirth + "<br>" +
-                                    "Account: <span style='color:red;'><strong>" + dto.UserName + "</strong></span><br>" + 
-                                    "Password: <span style='color:red;'><strong>" + dto.Password + "</strong></span>";
-                    EmailUtils.SendEmail(dto.ParentEmail!, toSubject, toContent);
-                    return;
-                }
+        if (account.Role != Constant.StaffRole)
+            throw new UnauthorizedException($"Not a staff role, Please login again!");
 
-                throw new BadRequestException("Email doesn't exist");
-            }
+        var order = await _unitOfWork.OrderRepository.GetByIdAsync(dto.OrderId);
+        var student = await _unitOfWork.StudentRepository.GetByIdAsync(dto.StudendId);
+        
+        if (student == null && order == null) 
             throw new BadRequestException($"StudentId:{dto.StudendId} not found");
-        }
-        throw new UnauthorizedException($"Not a staff role, Please login again!");
+
+        var parent = await _unitOfWork.AccountRepository.ExistByEmailAsync(order!.Parent!.Account.Email!);
+        if (parent == null) 
+            throw new BadRequestException("Email doesn't exist");
+        
+        //Create student account
+        student!.UserName = dto.UserName;
+        student.Account.PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(dto.Password);
+        _unitOfWork.StudentRepository.Update(student);
+        //Update order status to Success
+        order.Status = OrderStatus.Success;
+        _unitOfWork.OrderRepository.Update(order);
+        await _unitOfWork.SaveChangeAsync();
+        
+        //Send Email
+        var toSubject = "KidsPro Send Student Account";
+        var toContent = "Student Name: " + student.Account.FullName + "<br>" +
+                        "Birthday: " + student.Account.DateOfBirth + "<br>" +
+                        "Account: <span style='color:red;'><strong>" + dto.UserName + "</strong></span><br>" +
+                        "Password: <span style='color:red;'><strong>" + dto.Password + "</strong></span>";
+        EmailUtils.SendEmail(parent.Email!, toSubject, toContent);
+        
+        //Send Notify
+        var title = "The order "+order.OrderCode+" has been successfully confirmed";
+        var content = "Student account of "+student.Account.FullName+" send to email "+parent.Email;
+        await _notify.SendNotifyToAccountAsync(order.ParentId, title, content);
     }
 
-    public async Task<string> ViewReasonOrderCancel(int orderId,int parentId)
+    public async Task<string> ViewReasonOrderCancel(int orderId, int parentId)
     {
         // Check và lấy order vs requestRefund status
-        var order = await _unitOfWork.OrderRepository.GetOrderByStatusAsync(parentId, orderId,OrderStatus.RequestRefund);
-        return order?.Note??throw new BadRequestException($"OrderId: {orderId} not RequestRefund Status");
+        var order = await _unitOfWork.OrderRepository.GetOrderByStatusAsync(parentId, orderId,
+            OrderStatus.RequestRefund);
+        return order?.Note ?? throw new BadRequestException($"OrderId: {orderId} not RequestRefund Status");
     }
-    
 }
