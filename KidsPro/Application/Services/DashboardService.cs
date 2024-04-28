2afe7f7183bbe414f5780a61e9fcd15ee2f956fa
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2024-04-28 12:08:04 +0700

    fix dotsettings

---
+++ KidsPro/Application/Services/DashboardService.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata;
+using Application.ErrorHandlers;
+using Application.Interfaces.IServices;
+using Domain.Enums;
+using Constant = Application.Configurations.Constant;
+
+namespace Application.Services;
+
+public class DashboardService
+{
+    private IUnitOfWork _unitOfWork;
+    private IAccountService _accountService;
+
+    public DashboardService(IUnitOfWork unitOfWork, IAccountService accountService)
+    {
+        _unitOfWork = unitOfWork;
+        _accountService = accountService;
+    }
+
+    public async Task GetDashboardAsync()
+    {
+        var account = await _accountService.GetCurrentAccountInformationAsync();
+        if (account.Role != Constant.AdminRole)
+            throw new UnauthorizedException("Please login by account admin");
+
+        var orders = await _unitOfWork.OrderRepository.MobileGetListOrderAsync(OrderStatus.AllStatus,0,Constant.AdminRole);
+    }
+}
