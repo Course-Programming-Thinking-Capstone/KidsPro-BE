4e4886df3f6f29bd85e86dbc8a9221af4be53fac
thientm27 <denkhotieu1@gmail.com>
2024-04-29 18:41:08 +0700

    setup game controller test

---
+++ KidsPro/ParentTesting/GameControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Application;
+using Application.Interfaces.IRepositories;
+using Application.Interfaces.IServices;
+using Moq;
+using WebAPI.Controllers;
+
+namespace ParentTesting;
+
+[TestFixture]
+public class GameControllerTest
+{
+    private Mock<IAccountService> _mockService;
+    private Mock<IGameService> _mockGameService;
+    private Mock<IUnitOfWork> _mockUnitOfWork;
+    private GamesController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockService = new Mock<IAccountService>();
+        _mockGameService = new Mock<IGameService>();
+        _mockUnitOfWork = new Mock<IUnitOfWork>();
+        _controller = new GamesController(_mockService.Object, _mockGameService.Object);
+    }
+
+
+    
+}
